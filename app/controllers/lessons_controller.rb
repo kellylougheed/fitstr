622d1a576952cc31199d8c0e98236633ef1c82e0
Kelly Lougheed <kellylougheed@gmail.com>
2016-09-23 17:39:26 +0000

    Add permissions to lessons pages

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    current_course = current_lesson.section.course
+    if !current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be registered in this course to view the lessons'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
