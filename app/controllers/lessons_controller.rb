6c977c9f92e0118e7e3fc30bc4a3bf5777fce64f
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-02 20:36:08 +0000

    Allow instructor to see lessons without being enrolled in course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -9,7 +9,7 @@ class LessonsController < ApplicationController
 
   def require_authorized_for_current_course
     current_course = current_lesson.section.course
-    if !current_user.enrolled_in?(current_course)
+    if !current_user.enrolled_in?(current_course) && current_course.user != current_user
       redirect_to course_path(current_lesson.section.course), alert: 'You must be registered in this course to view the lessons'
     end
   end
